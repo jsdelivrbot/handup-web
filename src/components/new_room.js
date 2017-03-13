6b3610474652e5a0c95a1674cf916969e8348e55
marano <thiagomarano@gmail.com>
2017-03-12 22:38:59 -0300

    Missing semicolon.

--- src/components/new_room.js
+++ src/components/new_room.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
 import { ChangeRoomName } from '../actions';
 
 function NewRoom({ roomName, history, ChangeRoomName }) {
