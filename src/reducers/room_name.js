278079e48754802ed579cfde19adcf5100767e81
marano <thiagomarano@gmail.com>
2017-03-15 01:24:29 -0300

    Initialize room name with empty string.

--- src/reducers/room_name.js
+++ src/reducers/room_name.js
@@ -1,6 +1,6 @@
 import { CHANGE_ROOM_NAME } from '../actions';
 
-export default function RoomName(state = null, action) {
+export default function RoomName(state = '', action) {
   switch (action.type) {
     case CHANGE_ROOM_NAME:
       return action.payload;
