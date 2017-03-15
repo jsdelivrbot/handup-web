448cb55f5dff98117321e84a7045d321c1e754d7
marano <thiagomarano@gmail.com>
2017-03-14 21:37:49 -0300

    Declara mutations usando compose.

--- src/components/google_login.js
+++ src/components/google_login.js
@@ -2,31 +2,11 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { gql, graphql } from 'react-apollo';
+import { gql, graphql, compose } from 'react-apollo';
 import ReactGoogleLogin from 'react-google-login';
 import { googleClientId } from '../config';
 import { SetUserId, SetUserToken } from '../actions';
 
-@graphql(gql`
-  mutation login($input: LoginUserWithAuth0SocialInput!) {
-    loginUserWithAuth0Social(input: $input) {
-      token
-      user {
-        id
-        username
-      }
-    }
-  }
-`, { name: 'loginMutation' })
-@graphql(gql`
-  mutation updateUser($input: UpdateUserInput!) {
-    updateUser(input: $input) {
-      changedUser {
-        id
-      }
-    }
-  }
-`, { name: 'updateUserMutation' })
 class GoogleLogin extends Component {
   constructor(props) {
     super(props);
@@ -97,8 +77,35 @@ class GoogleLogin extends Component {
   }
 }
 
+const loginMutation = gql`
+  mutation login($input: LoginUserWithAuth0SocialInput!) {
+    loginUserWithAuth0Social(input: $input) {
+      token
+      user {
+        id
+        username
+      }
+    }
+  }
+`
+
+const updateUserMutation = gql`
+  mutation updateUser($input: UpdateUserInput!) {
+    updateUser(input: $input) {
+      changedUser {
+        id
+      }
+    }
+  }
+`
+
+const GoogleLoginWithData = compose(
+  graphql(loginMutation, { name: 'loginMutation' }),
+  graphql(updateUserMutation, { name: 'updateUserMutation' })
+)(GoogleLogin);
+
 function mapStateToProps({ returnTo }) {
   return { returnTo };
 };
 
-export default connect(mapStateToProps, { SetUserId, SetUserToken })(withRouter(GoogleLogin));
+export default connect(mapStateToProps, { SetUserId, SetUserToken })(withRouter(GoogleLoginWithData));
