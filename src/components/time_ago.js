a975658c70b65ace0c69e3431ce8dc7f8f9d45c6
marano <thiagomarano@gmail.com>
2017-03-16 14:49:13 -0300

    Atualiza tempo no contador.

--- src/components/time_ago.js
+++ src/components/time_ago.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 export default class TimeAgo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  }
+
+  componentWillMount() {
+    this.interval = setInterval(() => {
+      this.setState({ text: moment(this.props.time).fromNow() })
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <span>
-        {moment(this.props.time).fromNow()}
+        {this.state.text}
       </span>
     );
   }
