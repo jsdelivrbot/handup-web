2f62f9e56d93b35dc693124328f0ef12c93cf9b9
marano <thiagomarano@gmail.com>
2017-03-16 14:54:50 -0300

    Fix some redurcers function names.

--- src/reducers/is_creating_line_spot.js
+++ src/reducers/is_creating_line_spot.js
@@ -1,6 +1,6 @@
 import { SET_IS_CREATING_LINE_SPOT } from '../actions';
 
-export default function RoomName(state = false, action) {
+export default function IsCreatingLineSpot(state = false, action) {
   switch (action.type) {
     case SET_IS_CREATING_LINE_SPOT:
       return action.payload;
--- src/reducers/return_to.js
+++ src/reducers/return_to.js
@@ -1,6 +1,6 @@
 import { SET_RETURN_TO } from '../actions';
 
-export default function RoomName(state = null, action) {
+export default function ReturnTo(state = null, action) {
   switch (action.type) {
     case SET_RETURN_TO:
       return action.payload;
